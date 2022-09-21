f2acae9dd68eae74d09dcdd9bb7a05ee4ecd5b1c
jiw181 <97138877+jiw181@users.noreply.github.com>
2022-09-21 10:05:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pyclone-vi_jw', # added "_jw"
+    name='pyclone-vi-jw', # added "_jw"
     version='0.1.1',
     description='Fast method for inferring clonal population structure from SNV data.',
     author='Andrew Roth',
